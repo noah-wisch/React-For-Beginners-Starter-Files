d3f014ae70b4cead88bcc698585353d1c1fce53b
Noah Wisch <noah.wisch@gmail.com>
2017-04-17 14:06:49 -0400

    Pushing up progress for the first third of the videos

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './css/style.css'
+import { BrowserRouter, Match, Miss } from 'react-router';
 
+
+import './css/style.css'
 import App from './components/App'
+
 import StorePicker from './components/StorePicker'
+import NotFound from './components/NotFound'
+
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+    <div>
+        <Match  exactly pattern="/" component={StorePicker} />
+        <Match  pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
 
 render(<StorePicker/>, document.querySelector('#main'));
-render(<App/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'));
