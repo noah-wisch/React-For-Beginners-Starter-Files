a71a62a14bcb001fa5e8390e9c82e3cdd7c557c9
Noah Wisch <noah.wisch@gmail.com>
2017-04-21 13:04:32 -0400

    functioing remove button on inventory tab

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,8 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
 
@@ -58,6 +60,18 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
+  updateFish(key, updatedFish) {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+
+  removeFish(key) {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = null;
+    this.setState({ fishes });
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -88,7 +102,13 @@ class App extends React.Component {
           order={this.state.order}
           params={this.props.params}
         />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+      <Inventory
+        addFish={this.addFish}
+        removeFish={this.removeFish}
+        loadSamples={this.loadSamples}
+        fishes={this.state.fishes}
+        updateFish={this.updateFish}
+      />
       </div>
     )
   }
